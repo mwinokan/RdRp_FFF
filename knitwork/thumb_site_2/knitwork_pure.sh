ef8d319ace9e214cf8ee0de532eb8d099b304deb
Max Winokan <mwinokan@me.com>
2024-11-25 14:43:42 +0000

    progress

---
+++ knitwork/thumb_site_2/knitwork_pure.sh
@@ -0,0 +1,1 @@
+python $KNIT/Knitwork/runKnitting.py --substructure_pair_file fragment_output/substructure_pairs.json --pure_search --working_dir knitwork_temp --output_dir knitwork_pure_output --run_parallel --n_parallel 10 --limit 100 --clear_working_dir
