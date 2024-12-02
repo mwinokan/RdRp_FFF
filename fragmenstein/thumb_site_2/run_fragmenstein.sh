060f4d30589eb639026405cc26bdb4a745c79395
Max Winokan <mwinokan@me.com>
2024-12-02 13:40:19 +0000

    fstein inputs

---
+++ fragmenstein/thumb_site_2/run_fragmenstein.sh
@@ -0,0 +1,4 @@
+
+time fragmenstein laboratory combine -i *_hits.sdf -t ../../Flavi_NS5_RdRp/aligned_files/6izx-a/6izx-a_apo-desolv.pdb --victor Wictor
+
+# sb.sh --job-name "RdRp_fstein_cavB" $HOME2/slurm/run_bash_with_conda.sh run_fragmenstein.sh
